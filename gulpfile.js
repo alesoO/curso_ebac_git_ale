74b240374fa8e9460925251df9e1f586abdbbf49
Alexandre Soares <xxalesoaresxx@gmail.com>
2023-09-05 11:12:05 -0300

    new file:   build/img/html5.png 	new file:   build/js/script.js 	new file:   build/styles/main.css 	new file:   build/styles/maps/main.css.map 	deleted:    css/bootstrap.css 	new file:   gulpfile.js 	modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	renamed:    views/img/html5.png -> source/img/html5.png 	renamed:    js/script.js -> source/js/script.js 	renamed:    css/formulario.css -> source/styles/main.scss 	renamed:    css/reset.css -> source/styles/reset.scss 	new file:   source/styles/var.scss

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const sourceMaps = require('gulp-sourcemaps');
+const uglify = require('gulp-uglify');
+const obfuscate = require('gulp-obfuscate')
+const imagemin = require('gulp-imagemin')
+
+function compJs() {
+    return gulp.src('./source/js/*.js')
+        .pipe(uglify())
+        .pipe(obfuscate())
+        .pipe(gulp.dest('./build/js'));
+}
+
+function compImg() {
+    return gulp.src('./source/img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./build/img'));
+}
+
+function compSass() {
+    return gulp.src('./source/styles/main.scss')
+        .pipe(sourceMaps.init())
+        .pipe(sass({
+            outputStyle: 'compressed'
+        }))
+        .pipe(sourceMaps.write('./maps'))
+        .pipe(gulp.dest('./build/styles'));
+}
+
+exports.sass = compSass;
+exports.Js = compJs;
+exports.img = compImg;
+
+exports.watch = function () {
+    gulp.watch('./source/styles/*.scss', gulp.series(compSass));
+    gulp.watch('./source/js/*.js', gulp.series(compJs));
+    gulp.watch('./source/img/*', gulp.series(compImg));
+}
