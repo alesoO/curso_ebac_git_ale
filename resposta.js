1d0325904db19d15888c32ca4315c0937c773605
Alexandre <xxalesoaresxx@gmail.com>
2024-05-01 17:54:57 -0300

    modified:   resposta.js

--- resposta.js
+++ resposta.js
@@ -1,43 +1,34 @@
-class Carro {
-    constructor(modelo, fabricante, ano, placa) {
-        this.modelo = modelo;
-        this.fabricante = fabricante;
-        this.ano = ano;
-        this.placa = placa;
+const alunos = [
+    {
+        nome: "Jorge",
+        nota: 2,
+    },
+    {
+        nome: "Lucas",
+        nota: 7,
+    },
+    {
+        nome: "Pedro",
+        nota: 5,
+    },
+    {
+        nome: "Paula",
+        nota: 9,
+    },
+    {
+        nome: "Cassio",
+        nota: 4,
+    },
+    {
+        nome: "Ana",
+        nota: 6,
     }
-    test() {
-        console.log(`O carro ${this.fabricante} ${this.modelo} funciona.`);
-    }
+]
+
+function media(alunos){
+    return alunos.filter(function(item){
+        return item.nota >= 6;
+    })
 }
 
-class Moto extends Carro {
-    constructor(modelo, fabricante, ano, placa, roubada) {
-        super(modelo, fabricante, ano, placa);
-        this.roubada = roubada;
-    }
-    test() {
-        console.log(`O carro ${this.fabricante} ${this.modelo} funciona.`);
-    }
-}
-
-class Sucata extends Carro {
-    constructor(modelo, fabricante, ano, placa, tipoDeDano) {
-        super(modelo, fabricante, ano, placa);
-        this.tipoDeDano = tipoDeDano;
-    }
-    test() {
-        console.log(`O carro ${this.fabricante} ${this.modelo} possui peças ultilizaveis.`);
-    }
-}
-
-const fiatCarro = new Carro("Uno", "Fiat", 2009, "KFW-1235");
-const FordCarro = new Carro("Ka", "Ford", 2014, "OLW-1737");
-const mazdaCarro = new Carro("MX5", "Mazda", 1994, "THB-7452");
-const hondaMoto = new Moto("CG", "Honda", 2020, "HGE-6248", false);
-const sucata = new Sucata("Palio", "Fiat", 2002, "PJY-7862", "Capotamento");
-
-fiatCarro.test();
-FordCarro.test();
-mazdaCarro.test();
-hondaMoto.test();
-sucata.test();
+console.log(media(alunos));
