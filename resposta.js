b967530901deb07e128ca36a3f99bfc1c37f446f
Alexandre <xxalesoaresxx@gmail.com>
2024-06-07 18:45:34 -0300

    Exercício Módulo 26

--- resposta.js
+++ resposta.js
@@ -1,34 +1,7 @@
-const alunos = [
-    {
-        nome: "Jorge",
-        nota: 2,
-    },
-    {
-        nome: "Lucas",
-        nota: 7,
-    },
-    {
-        nome: "Pedro",
-        nota: 5,
-    },
-    {
-        nome: "Paula",
-        nota: 9,
-    },
-    {
-        nome: "Cassio",
-        nota: 4,
-    },
-    {
-        nome: "Ana",
-        nota: 6,
-    }
-]
-
-function media(alunos){
-    return alunos.filter(function(item){
-        return item.nota >= 6;
-    })
+"use strict";
+function multiply(n1, n2) {
+    return n1 * n2;
 }
-
-console.log(media(alunos));
+function greeting(name) {
+    return "Olá" + name;
+}
---
+++ resposta.ts
@@ -0,0 +1,7 @@
+function multiply(n1: number, n2: number): number {
+    return n1 * n2;
+}
+
+function greeting(name: string): string {
+    return "Olá" + name;
+}
